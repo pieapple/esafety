44abafdc17c13ee8e106f787fe718308fae16723
Zhihong Liu <pieapple03@gmail.com>
2012-07-02 22:38:23 +0800

    add some more controller pages

--- src/etraining/admin_views.py
+++ src/etraining/admin_views.py
@@ -58,7 +58,8 @@ def training_signup(request):
   if request.method == "POST":
 # get list of ids
 # associate and set attendtime/admin for each training item
-    return render_to_response("etraining/admin/employee_registration_result.html", {}, context_instance=RequestContext(request))
+# Ajax call to do signup
+    pass
   else: 
 # query training
     return render_to_response("etraining/admin/training_signup.html", {}, context_instance=RequestContext(request))
@@ -67,107 +68,64 @@ def training_signup(request):
 def view_training(request):
 # get training by id
   return render_to_response("etraining/admin/view_training.html", {
-      'training': training,
     }, context_instance=RequestContext(request))
 
 
 @login_required
 def schedule_employee_training(request):
   if request.method == "POST":
-    form = VisitorRegistrationForm(request.POST)
-    if form.is_valid():
-      # notify user to display entrance training
-      return HttpResponseRedirect(reverse("visitor_training"))
-  else:
-    form = VisitorRegistrationForm()
+    pass
 
-  return render_to_response("etraining/admin/schedule_employee_training.html", {
-      'form': form,
-    }, context_instance=RequestContext(request))
+  return render_to_response("etraining/admin/schedule_employee_training.html", {}, context_instance=RequestContext(request))
 
 @login_required
 def schedule_vendor_training(request):
   if request.method == "POST":
-    form = VisitorRegistrationForm(request.POST)
-    if form.is_valid():
-# notify user to display entrance training
-      return HttpResponseRedirect(reverse("visitor_training"))
-  else:
-    form = VisitorRegistrationForm()
+    pass
 
-  return render_to_response("etraining/admin/schedule_employee_training.html", {
-      'form': form,
-    }, context_instance=RequestContext(request))
+  return render_to_response("etraining/admin/schedule_vendor_training.html", {}, context_instance=RequestContext(request))
 
 @login_required
 def schedule_entrance_training(request):
   if request.method == "POST":
-    form = VisitorRegistrationForm(request.POST)
-    if form.is_valid():
-# notify user to display entrance training
-      return HttpResponseRedirect(reverse("visitor_training"))
-  else:
-    form = VisitorRegistrationForm()
+    pass
 
-  return render_to_response("etraining/admin/schedule_employee_training.html", {
-      'form': form,
-    }, context_instance=RequestContext(request))
+  return render_to_response("etraining/admin/schedule_visitor_training.html", {}, context_instance=RequestContext(request))
 
 @login_required
 def view_employee_training(request):
-  if request.method == "POST":
-    return render_to_response("etraining/admin/view_employee_training.html", {}, context_instance=RequestContext(request))
-  else:
-    return render_to_response("etraining/admin/view_employee_training.html", {}, context_instance=RequestContext(request))
+  return render_to_response("etraining/admin/view_employee_training.html", {}, context_instance=RequestContext(request))
 
 @login_required
 def view_employee_training_detail(request):
-  if request.method == "POST":
-    return render_to_response("etraining/admin/view_employee_training.html", {}, context_instance=RequestContext(request))
-  else:
-    return render_to_response("etraining/admin/view_employee_training.html", {}, context_instance=RequestContext(request))
+  return render_to_response("etraining/admin/view_employee_training_detail.html", {}, context_instance=RequestContext(request))
 
 @login_required
 def view_vendor_training(request):
-  if request.method == "POST":
-    return render_to_response("etraining/admin/view_vendor_training.html", {}, context_instance=RequestContext(request))
-  else:
-    return render_to_response("etraining/admin/view_vendor_training.html", {}, context_instance=RequestContext(request))
+  return render_to_response("etraining/admin/view_vendor_training.html", {}, context_instance=RequestContext(request))
 
 @login_required
 def view_vendor_training_detail(request):
-  if request.method == "POST":
-    return render_to_response("etraining/admin/view_vendor_training_detail.html", {}, context_instance=RequestContext(request))
-  else:
-    return render_to_response("etraining/admin/view_vendor_training_detail.html", {}, context_instance=RequestContext(request))
+  return render_to_response("etraining/admin/view_vendor_training_detail.html", {}, context_instance=RequestContext(request))
 
 @login_required
 def view_training_signup(request):
-  if request.method == "POST":
-    return render_to_response("etraining/admin/view_training_signup.html", {}, context_instance=RequestContext(request))
-  else:
-    return render_to_response("etraining/admin/view_training_signup.html", {}, context_instance=RequestContext(request))
+  return render_to_response("etraining/admin/view_training_signup.html", {}, context_instance=RequestContext(request))
 
 @login_required
 def view_visitor_entrance(request):
-  if request.method == "POST":
-    return render_to_response("etraining/admin/view_training_signup.html", {}, context_instance=RequestContext(request))
-  else:
-    return render_to_response("etraining/admin/view_training_signup.html", {}, context_instance=RequestContext(request))
+  return render_to_response("etraining/admin/view_visitor_entrance.html", {}, context_instance=RequestContext(request))
 
 @login_required
 def view_visitor_entrance_detail(request):
-  if request.method == "POST":
-    return render_to_response("etraining/admin/view_training_signup.html", {}, context_instance=RequestContext(request))
-  else:
-    return render_to_response("etraining/admin/view_training_signup.html", {}, context_instance=RequestContext(request))
+  return render_to_response("etraining/admin/view_visitor_entrance_detail.html", {}, context_instance=RequestContext(request))
 
 @login_required
-def manage_document(request):
+def manage_documents(request):
   if request.method == "POST":
-    return render_to_response("etraining/admin/view_training_signup.html", {}, context_instance=RequestContext(request))
+    return render_to_response("etraining/admin/manage_documents.html", {}, context_instance=RequestContext(request))
   else:
-    return render_to_response("etraining/admin/view_training_signup.html", {}, context_instance=RequestContext(request))
+    return render_to_response("etraining/admin/manage_documents.html", {}, context_instance=RequestContext(request))
 
 @login_required
 def add_document(request):
@@ -184,9 +142,9 @@ def delete_document(request):
 @login_required
 def manage_question_poll(request):
   if request.method == "POST":
-    return render_to_response("etraining/admin/view_training_signup.html", {}, context_instance=RequestContext(request))
+    return render_to_response("etraining/admin/manage_question_poll.html", {}, context_instance=RequestContext(request))
   else:
-    return render_to_response("etraining/admin/view_training_signup.html", {}, context_instance=RequestContext(request))
+    return render_to_response("etraining/admin/manage_question_poll.html", {}, context_instance=RequestContext(request))
 
 @login_required
 def add_question_poll(request):
@@ -207,13 +165,13 @@ def delete_question(request):
 @login_required
 def manage_employee_org(request):
   if request.method == "POST":
-    return render_to_response("etraining/admin/view_training_signup.html", {}, context_instance=RequestContext(request))
+    return render_to_response("etraining/admin/manage_employee_org.html", {}, context_instance=RequestContext(request))
   else:
-    return render_to_response("etraining/admin/view_training_signup.html", {}, context_instance=RequestContext(request))
+    return render_to_response("etraining/admin/manage_employee_org.html", {}, context_instance=RequestContext(request))
 
 @login_required
 def manage_visitor_org(request):
   if request.method == "POST":
-    return render_to_response("etraining/admin/view_training_signup.html", {}, context_instance=RequestContext(request))
+    return render_to_response("etraining/admin/manage_visitor_org.html", {}, context_instance=RequestContext(request))
   else:
-    return render_to_response("etraining/admin/view_training_signup.html", {}, context_instance=RequestContext(request))
+    return render_to_response("etraining/admin/manage_visitor_org.html", {}, context_instance=RequestContext(request))
